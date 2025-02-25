1864ae2bd38592b5ff5af8cb73243f7247f6819d
dtombot <tomselfdevelopr@gmail.com>
2025-02-25 17:05:03 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Blog from './components/Blog';
+import ToolForm from './components/ToolForm';
+
 function App() {
   return (
     <div>
-      <h1>Welcome to DomainDiscoverer</h1>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/submit-tool" element={<ToolForm />} />
+      </Routes>
     </div>
   );
 }
