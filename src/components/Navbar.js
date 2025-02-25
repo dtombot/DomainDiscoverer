c96ed9a459611543905e89ad17a0766e3c19a2e2
dtombot <tomselfdevelopr@gmail.com>
2025-02-25 17:27:36 +0100

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,8 +2,11 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav>
-      <Link to="/">Home</Link> | <Link to="/blog">Blog</Link>
+    <nav style={{ padding: '10px', background: '#f8f9fa' }}>
+      <Link to="/" style={{ marginRight: '10px' }}>Home</Link> | 
+      <Link to="/blog" style={{ marginRight: '10px' }}>Blog</Link> | 
+      <Link to="/submit-tool" style={{ marginRight: '10px' }}>Submit Tool</Link> | 
+      <Link to="/admin">Admin</Link>
     </nav>
   );
 }
