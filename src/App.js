49388b47b70df20dd2360376222039837c3ac862
dtombot <tomselfdevelopr@gmail.com>
2025-02-25 18:06:43 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './components/AuthContext';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Blog from './components/Blog';
@@ -8,18 +9,20 @@ import Footer from './components/Footer';
 
 function App() {
   return (
-    <div style={{ minHeight: '100vh', display: 'flex', flexDirection: 'column' }}>
-      <Navbar />
-      <div style={{ flex: '1' }}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/blog" element={<Blog />} />
-          <Route path="/submit-tool" element={<ToolForm />} />
-          <Route path="/admin" element={<AdminDashboard />} />
-        </Routes>
+    <AuthProvider>
+      <div style={{ minHeight: '100vh', display: 'flex', flexDirection: 'column' }}>
+        <Navbar />
+        <div style={{ flex: '1' }}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/blog" element={<Blog />} />
+            <Route path="/submit-tool" element={<ToolForm />} />
+            <Route path="/admin" element={<AdminDashboard />} />
+          </Routes>
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </AuthProvider>
   );
 }
 
