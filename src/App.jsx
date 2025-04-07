1adacb2756c5f3dde035ad291383f3ea928bbb55
dtombot <tomselfdevelopr@gmail.com>
2025-04-07 13:01:52 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { Routes, Route } from 'react-router-dom';
-import { AuthProvider } from './components/AuthContext';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Blog from './components/Blog';
@@ -11,22 +10,20 @@ import ToolDetails from './components/ToolDetails';
 
 function App() {
   return (
-    <AuthProvider>
-      <div className="min-h-screen flex flex-col bg-primary">
-        <Navbar />
-        <main className="flex-1">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/blog" element={<Blog />} />
-            <Route path="/submit-tool" element={<ToolForm />} />
-            <Route path="/admin" element={<AdminDashboard />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/tool/:id" element={<ToolDetails />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </AuthProvider>
+    <div className="min-h-screen flex flex-col bg-primary">
+      <Navbar />
+      <main className="flex-1">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/submit-tool" element={<ToolForm />} />
+          <Route path="/admin" element={<AdminDashboard />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/tool/:id" element={<ToolDetails />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
   );
 }
 
